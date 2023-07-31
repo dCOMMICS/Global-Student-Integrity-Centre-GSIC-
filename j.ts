3e3fb464a47bdb29aa285695a2b0690753e1969c
Craig Wurld <85875550+dCOMMICS@users.noreply.github.com>
2023-07-31 15:28:39 +0300

    🔥🤞

--- j.ts
+++ j.ts
@@ -3,3 +3,13 @@ function getTypeScriptCompilerOptions(src: string): ts.CompilerOptions {
 	const rootDir = path.join(__dirname, `../../${src}`);
 	let options: ts.CompilerOptions = {};
 	options.verbose = false;
+
+    // omo oluwatusin // 
+
+    const body = await net.fetch('test-scheme://foo', {
+        method: 'POST',
+        body: getWebStream(),
+        duplex: 'half' // https://github.com/microsoft/TypeScript/issues/53157
+      } as any).then(r => r.text());
+      expect(body).to.equal(text);
+    });
